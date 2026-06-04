ab772b581ff286e6f2bf6eff9db1c8d75ee41655
SAMBATHKUMAR S <131866247+sambathsaga@users.noreply.github.com>
2026-06-04 12:16:41 +0530

    Create or_gate_tb.v

---
+++ gates/or_gate/or_gate_tb.v
@@ -0,0 +1,16 @@
+module or_gate_tb;
+reg a,b;
+wire y;
+or_gate uut(.a(a),.b(b),.y(y));
+initial begin
+$dumpfile ("wave.vcd");
+$dumpvars (0,or_gate_tb);
+$display ("A B| Y");
+$monitor("%b %b|%b",a,b,y);
+a=0;b=0;#10;
+a=0;b=1;#10;
+a=1;b=0;#10;
+a=1;b=1;#10;
+$finish;
+end
+endmodule
