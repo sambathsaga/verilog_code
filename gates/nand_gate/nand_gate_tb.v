0fe5254ac47518eed6644f4b307dbcd366488f0a
SAMBATHKUMAR S <131866247+sambathsaga@users.noreply.github.com>
2026-06-04 13:59:45 +0530

    Create nand_gate_tb.v

---
+++ gates/nand_gate/nand_gate_tb.v
@@ -0,0 +1,16 @@
+module nand_gate_tb;
+reg a,b;
+wire y;
+nand_gate uut(.a(a),.b(b),.y(y));
+initial begin
+$dumpfile ("wave.vcd");
+$dumpvars(0,nand_gate_tb);
+$display (" A  B | Y");
+$monitor (" %b %b | %b",a,b,y);
+a=0;b=0;#10
+a=0;b=1;#10;
+a=1;b=0;#10;
+a=1;b=1;#10;
+$finish;
+end
+endmodule
